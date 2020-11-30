ed37fe09f4290c3196cd8a2d16a011baf10e6db4
Daniel Hilst Selli <daniel@versatushpc.com.br>
2020-11-29 21:08:08 -0300

    Move tests to test.py

--- test.py
+++ test.py
@@ -2,11 +2,11 @@
 import sys
 import doctest
 import unittest
-from pathlib import Path
-from importlib import import_module
 
+from pyparsing import Word, nums  # type: ignore
 
 from lampy import lampy, utils
+from lampy.lampy import BNF, arit_BNF
 
 
 def load_tests(loader, tests, ignore):
@@ -14,3 +14,40 @@ def load_tests(loader, tests, ignore):
     tests.addTests(doctest.DocTestSuite(utils))
     return tests
 
+
+class Test(unittest.TestCase):
+    def test_bnf(self):
+        BNF().runTests(
+            """
+           # Simple abstraction
+           fn x => x y y
+
+           # Chainned abstrxction
+           fn x => fn y => x y
+
+           # Abstraction application
+           (fn x => x y) (fn x => x)
+
+           # Try left associativity of appliction
+           u v w x y z
+
+           # Simple xpplicxtion
+           (fn x => x) a
+
+           # ɑ conversion needed
+           (fn x => x y) a
+
+           # Value
+           1
+
+           # Parenthesis
+           x z (y z)
+
+           """
+        )
+
+        arit_BNF(Word(nums)).runTests(
+            """
+            1 * (2 + 3)
+            """
+        )
