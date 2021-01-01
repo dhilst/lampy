46f231bc7935497c0538959a88112c8a7d9b2f6b
Daniel Hilst Selli <daniel@versatushpc.com.br>
2021-01-01 15:31:31 -0300

    Uncomment bar.py

--- bar.py
+++ bar.py
@@ -4,46 +4,55 @@ from ast import parse, fix_missing_locations, Expression, NodeTransformer, parse
 from astlib import lazy, unify, match
 from letast import matchdec
 
-# def sum(*values):
-#     return match(values,
-#             ("[]", lambda: 0),
-#             ("a, *b", lambda a, b: a + sum(*b)))
-#
-#
-# def fact(n):
-#     return match(n,
-#             ("0", lambda: 1),
-#             ("n", lambda n: n * fact(n-1)))
-#
-# def map_(cb, it):
-#     return match(it,
-#             ("[]", lambda: []),
-#             ("a, *b", lambda a, b: [cb(a)] + map_(cb, b)))
-#
-#
-# class Bar:
-#     bar = 1
-#
-# class Foo:
-#     bar = Bar()
-#     zar = "hello"
-#
-# match(Foo(),
-#     # object destruction
-#     ("Foo(zar, bar=Bar(bar=1))", lambda zar: print(f"{zar} pattern match")),
-#     ("_", lambda: print("whaever")))
-#
-# print(
-#     match((False, False),
-#         ("True,_",  lambda : f"True  ?"),
-#         ("False,_", lambda : f"False ?"))
-# )
-#
-#
-# print(sum(1, 2, 3, 4))
-# print(fact(5))
-# print(map_(str.upper, ["foo", "bar"]))
-#
+def sum(*values):
+    return match(values,
+            ("[]", lambda: 0),
+            ("a, *b", lambda a, b: a + sum(*b)))
+
+
+def fact(n):
+    return match(n,
+            ("0", lambda: 1),
+            ("n", lambda n: n * fact(n-1)))
+
+def map_(cb, it):
+    return match(it,
+            ("[]", lambda: []),
+            ("a, *b", lambda a, b: [cb(a)] + map_(cb, b)))
+
+
+def zip_(a_it, b_it):
+    return match(a_it,
+            ("[]", lambda: []),
+            ("a,*b", lambda a, b: \
+                match(b_it,
+                ("[]", lambda: []),
+                ("c,*d", lambda c, d: [(a,c)] + zip_(b,d)))))
+print(zip_([1, 2, 3],["a", "b", "c"])) # => [(1, 'a'), (2, 'b'), ...]
+
+class Bar:
+    bar = 1
+
+class Foo:
+    bar = Bar()
+    zar = "hello"
+
+match(Foo(),
+    # object destruction
+    ("Foo(zar, bar=Bar(bar=1))", lambda zar: print(f"{zar} pattern match")),
+    ("_", lambda: print("whaever")))
+
+print(
+    match((False, False),
+        ("True,_",  lambda : f"True  ?"),
+        ("False,_", lambda : f"False ?"))
+)
+
+
+print(sum(1, 2, 3, 4))
+print(fact(5))
+print(map_(str.upper, ["foo", "bar"]))
+
 
 from enum import Enum
 class E(Enum):
@@ -54,6 +63,13 @@ class E(Enum):
 match(E.B,
         ("E.A", lambda : print("A")),
         ("E.B", lambda : print("B")),
-        ("_", lambda : print("foda-se")),
+        ("E.C", lambda : print("C")),
         )
 
+try:
+    match(E.B,
+            ("E.A", lambda : print("A")),
+            ("E.B", lambda : print("B")),
+            )
+except TypeError as e: # because match is not exaustive
+    print(e)
